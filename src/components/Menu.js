dda68ff517bc880b386416f4c9a7d48da87f2801
mfee06paradis <64397614+mfee06paradis@users.noreply.github.com>
2020-04-30 15:51:32 +0800

    Update Menu
    無RWD的Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,90 +1,153 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
+import {
+  Modal,
+  Row,
+  Col,
+  Button,
+  InputGroup,
+  FormControl,
+} from 'react-bootstrap'
+import '../styles/navbar.scss'
+
+function LoginModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+      id="login-modal"
+    >
+      <Modal.Header></Modal.Header>
+      <Modal.Body>
+        <InputGroup className="mb-2">
+          <FormControl
+            placeholder="email"
+            aria-label="email"
+            aria-describedby="login-email"
+          />
+        </InputGroup>
+        <InputGroup className="mb-2">
+          <InputGroup.Prepend>
+            <InputGroup.Text id="login-password">@</InputGroup.Text>
+          </InputGroup.Prepend>
+          <FormControl
+            placeholder="password"
+            aria-label="password"
+            aria-describedby="login-password"
+          />
+        </InputGroup>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  )
+}
 
 function Menu() {
+  const [modalShow, setModalShow] = React.useState(false)
+
   return (
     <>
-      {/* <div className="navbar">
-        <div className="col-5 nav-outline">
-          <div className="nav-link">
-            <span className="col-6">
-              <span class="nav-title-eng">BRANDS</span>
-              <span class="nav-title-chs">品牌</span>
-            </span>
-            <span className="col-6">
-              <span class="nav-title-eng">PRODUCTS</span>
-            </span>
-          </div>
-        </div>
-        <div className="col-2">LOGO</div>
-        <div className="col-5 nav-outline">
-          <div className="nav-link">
-            <span className="col-6">
-              <span class="nav-title-eng">DISCOUNT</span>
-            </span>
-            <span className="col-6">
-              <span class="nav-title-eng">MAKEUP</span>
-            </span>
-          </div>
-        </div>
-      </div> */}
-
-      <nav className="navbar navbar-expand-lg">
-        <div className="top-nav-center">
-          <NavLink className="navbar-brand" to="/">
-            logo
+      <div className="navbar-bg"></div>
+      <Row className="navbar-icons">
+        <Col className="navbar-search">
+          <NavLink
+            activeClassName="active"
+            className="nav-link"
+            to="/Uielements"
+          ></NavLink>
+        </Col>
+        <Col className="navbar-cart">
+          <NavLink
+            activeClassName="active"
+            className="nav-link"
+            to="/Cart"
+          ></NavLink>
+        </Col>
+        <Col className="navbar-favorite">
+          <NavLink
+            activeClassName="active"
+            className="nav-link"
+            to="/Uielements"
+          ></NavLink>
+        </Col>
+        <Col className="navbar-member" md={4}>
+          <NavLink
+            activeClassName="active"
+            className="nav-link"
+            to="/Uielements"
+            onClick={() => setModalShow(true)}
+          >
+            <p className="navbar-text-span">會員登入</p>
           </NavLink>
-        </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
-            <li className="nav-item">
+        </Col>
+        <Col className="navbar-logout">
+          <NavLink
+            activeClassName="active"
+            className="nav-link"
+            to="/Uielements"
+          ></NavLink>
+        </Col>
+      </Row>
+      <NavLink className="navbar-logo" to="/">
+        <div className="logo"></div>
+      </NavLink>
+      <Row className="navbar">
+        <Col md={5} className="nav-outline">
+          <Row className="nav-inline">
+            <Col>
               <NavLink
-                exact
                 activeClassName="active"
                 className="nav-link"
                 to="/Uielements"
               >
-                UIelements
+                <p class="nav-title-eng">BRANDS</p>
+                <p class="nav-title-chs">品牌</p>
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink
-                exact
-                activeClassName="active"
-                className="nav-link"
-                to="/"
-              >
-                產品列表
-              </NavLink>
-            </li>
-            <li className="nav-item">
+            </Col>
+            <Col>
               <NavLink
                 activeClassName="active"
                 className="nav-link"
                 to="/product"
               >
-                產品
+                <p class="nav-title-eng">PRODUCTS</p>
+                <p class="nav-title-chs">商品</p>
               </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink activeClassName="active" className="nav-link" to="/cart">
-                購物車
+            </Col>
+          </Row>
+        </Col>
+        <Col md={2}>LOGO</Col>
+        <Col md={5} className="nav-outline">
+          <Row className="nav-inline">
+            <Col>
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to="/product"
+              >
+                <p class="nav-title-eng">DISCOUNT</p>
+                <p class="nav-title-chs">優惠</p>
               </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
+            </Col>
+            <Col>
+              <NavLink
+                activeClassName="active"
+                className="nav-link"
+                to="/product"
+              >
+                <p class="nav-title-eng">MAKEUP</p>
+                <p class="nav-title-chs">彩妝教學</p>
+              </NavLink>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+
+      <LoginModal show={modalShow} onHide={() => setModalShow(false)} />
     </>
   )
 }
